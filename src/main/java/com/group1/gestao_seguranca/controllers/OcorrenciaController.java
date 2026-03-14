1eb2cfa7a5db4e702942fe78645f540d94fb0c40
zWellingtonLima <0wellingtonlima1@gmail.com>
2026-03-14 14:26:27 +0000

    cria Ocorrencias e TipoConsumo Controller

--- src/main/java/com/group1/gestao_seguranca/controllers/OcorrenciaController.java
+++ src/main/java/com/group1/gestao_seguranca/controllers/OcorrenciaController.java
@@ -1,27 +1,23 @@
 package com.group1.gestao_seguranca.controllers;
 
 import com.group1.gestao_seguranca.entities.*;
-import com.group1.gestao_seguranca.enums.TipoOcorrenciaEnum;
 import com.group1.gestao_seguranca.repositories.OcorrenciasRepository;
 import com.group1.gestao_seguranca.repositories.UsersRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/ocorrencias")
 public class OcorrenciaController {
 
-    @Autowired
-    OcorrenciasRepository ocorrenciasRepo;
-    @Autowired
-    UsersRepository usersRepo;
+    private final OcorrenciasRepository ocorrenciasRepo;
+    private final UsersRepository usersRepo;
+
+    public OcorrenciaController(OcorrenciasRepository ocorrenciasRepo, UsersRepository usersRepo) {
+        this.ocorrenciasRepo = ocorrenciasRepo;
+        this.usersRepo = usersRepo;
+    }
 
     @GetMapping
     public List<Ocorrencias> list() {
@@ -34,19 +30,5 @@ public class OcorrenciaController {
                 .orElseThrow();
         ocorrencias.setSeguranca(user);
         return ocorrenciasRepo.save(ocorrencias);
-    }
-
-    @GetMapping("/tipos")
-    public ResponseEntity<?> getTipos() {
-        List<Map<String, String>> tipos = Arrays.stream(TipoOcorrenciaEnum.values())
-                .map(tipo -> {
-                    Map<String, String> item = new LinkedHashMap<>();
-                    item.put("valor", tipo.name());
-                    item.put("label", tipo.getLabel());
-                    return item;
-                })
-                .collect(Collectors.toList());
-
-        return ResponseEntity.ok(tipos);
     }
 }
--- src/main/java/com/group1/gestao_seguranca/controllers/TipoConsumoController.java
+++ src/main/java/com/group1/gestao_seguranca/controllers/TipoConsumoController.java
@@ -2,36 +2,38 @@ package com.group1.gestao_seguranca.controllers;
 
 import com.group1.gestao_seguranca.entities.TipoConsumo;
 import com.group1.gestao_seguranca.repositories.TipoConsumoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-    @RequestMapping("/api/tipo-consumo")
-    public class TipoConsumoController {
+@RequestMapping("/api/tipo-consumo")
+public class TipoConsumoController {
 
-        @Autowired
-        private TipoConsumoRepository tipoConsumoRepo;
+    private final TipoConsumoRepository tipoConsumoRepo;
 
-        @GetMapping
-        public List<TipoConsumo> list() {
-            return tipoConsumoRepo.findAll();
-        }
-
-        @PostMapping
-        public TipoConsumo create(@RequestBody TipoConsumo tipo) {
-            return tipoConsumoRepo.save(tipo);
-        }
-
-        @GetMapping("/{id}")
-        public TipoConsumo search(@PathVariable Integer id) {
-            return tipoConsumoRepo.findById(id).orElse(null);
-        }
-
-        @DeleteMapping("/{id}")
-        public void delete(@PathVariable Integer id) {
-            tipoConsumoRepo.deleteById(id);
-        }
+    public TipoConsumoController(TipoConsumoRepository tipoConsumoRepo) {
+        this.tipoConsumoRepo = tipoConsumoRepo;
     }
+
+    @GetMapping
+    public List<TipoConsumo> list() {
+        return tipoConsumoRepo.findAll();
+    }
+
+    @PostMapping
+    public TipoConsumo create(@RequestBody TipoConsumo tipo) {
+        return tipoConsumoRepo.save(tipo);
+    }
+
+    @GetMapping("/{id}")
+    public TipoConsumo search(@PathVariable Integer id) {
+        return tipoConsumoRepo.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Integer id) {
+        tipoConsumoRepo.deleteById(id);
+    }
+}
 
