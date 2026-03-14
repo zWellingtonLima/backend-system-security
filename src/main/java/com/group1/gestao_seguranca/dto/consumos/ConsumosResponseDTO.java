bd63952804e84505eb373c6530d878178c6bc6da
zWellingtonLima <0wellingtonlima1@gmail.com>
2026-03-14 13:06:58 +0000

    adiciona Interceptor e WebConfig para tratar Sessoes

---
+++ src/main/java/com/group1/gestao_seguranca/dto/consumos/ConsumosResponseDTO.java
@@ -0,0 +1,4 @@
+package com.group1.gestao_seguranca.dto.consumos;
+
+public class ConsumosResponseDTO {
+}
---
+++ src/main/java/com/group1/gestao_seguranca/services/ConsumosService.java
@@ -0,0 +1,4 @@
+package com.group1.gestao_seguranca.services;
+
+public class ConsumosService {
+}
