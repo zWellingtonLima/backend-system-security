9b86d33a3c9e1758dbe89be903e652c16e95ab5b
zWellingtonLima <0wellingtonlima1@gmail.com>
2026-03-23 11:41:35 +0000

    adiciona método PATCH ao campo de `allowedMethods` no CorsConfig

--- src/main/java/com/group1/gestao_seguranca/config/CorsConfig.java
+++ src/main/java/com/group1/gestao_seguranca/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedMethods("GET", "PATCH", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
         };
